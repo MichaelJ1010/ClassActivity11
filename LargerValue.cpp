ddf32aac140f11368695280e93800def3a16653a
Michael J <30031129@cc.peralta.edu>
2024-09-15 19:44:41 -0700

    simple if statement to find largest value

--- LargerValue.cpp
+++ LargerValue.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main(){
+    int valueOne,valueTwo;
+    int largerValue;
 
+    cout << "Please write down two numbers:" << endl;
+    cin >> valueOne >> valueTwo;
+    if(valueOne > valueTwo){
+        largerValue = valueOne;
+    }else{
+        largerValue = valueTwo;
+    }
+    cout << "the larger value was " << largerValue;
     return 0;
 }
