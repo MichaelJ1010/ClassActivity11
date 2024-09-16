7172702d377cc5881a7b4a95e8c01e4ab4e3abe5
Michael J <30031129@cc.peralta.edu>
2024-09-15 19:55:02 -0700

    added logical operator

--- LargerValue.cpp
+++ LargerValue.cpp
@@ -8,11 +8,14 @@ int main(){
 
     cout << "Please write down two numbers:" << endl;
     cin >> valueOne >> valueTwo;
-    if(valueOne > valueTwo){
-        largerValue = valueOne;
-    }else{
-        largerValue = valueTwo;
-    }
+    // if(valueOne > valueTwo){
+    //     largerValue = valueOne;
+    // }else{
+    //     largerValue = valueTwo;
+    // }
+
+    largerValue = (valueOne > valueTwo) ? valueOne : valueTwo;
+    
     cout << "the larger value was " << largerValue;
     return 0;
 }
